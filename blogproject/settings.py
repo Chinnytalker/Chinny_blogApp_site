8bf302eb29b2ac7eb49112b6301df89ea1cdf584
Nwachukwu Clinton <nwachukwuclinton2@gmail.com>
2024-10-02 23:55:59 +0100

    added debug false

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogApp.apps.BlogappConfig',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -162,4 +164,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 django_heroku.settings(locals())
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dseh4nwkz',
+    'API_KEY': '819489763854264',
+    'API_SECRET': 'd7g7GCSx2E6MdBeGZOsGabX0QAM',
+}
+
+DEFAULT_FILE_STORAGE = 'cloundinary_storage.storage.MediaCloudinaryStorage'
+
+MEDIA_URL = 'https://res.cloudinary.com/dseh4nwkz/'
 
