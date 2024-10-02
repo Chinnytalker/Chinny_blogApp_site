778fbee7ce80cb40cd76afb958d9b7a5a77f9a2e
Nwachukwu Clinton <nwachukwuclinton2@gmail.com>
2024-10-03 00:41:17 +0100

    added cloundinary 2

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -166,13 +166,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
 
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': 'dseh4nwkz',
-#     'API_KEY': '819489763854264',
-#     'API_SECRET': 'd7g7GCSx2E6MdBeGZOsGabX0QAM',
-# }
-#
-# DEFAULT_FILE_STORAGE = 'cloundinary_storage.storage.MediaCloudinaryStorage'
-#
-# MEDIA_URL = 'https://res.cloudinary.com/dseh4nwkz/'
-#
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dseh4nwkz',
+    'API_KEY': '819489763854264',
+    'API_SECRET': 'd7g7GCSx2E6MdBeGZOsGabX0QAM',
+}
+
+DEFAULT_FILE_STORAGE = 'cloundinary_storage.storage.MediaCloudinaryStorage'
+
+MEDIA_URL = 'https://res.cloudinary.com/dseh4nwkz/'
+
